e56822bee84ee92f2c7d3a5a99b5814509dbf780
fenita.03 <facunico145@gmail.com>
2026-04-16 19:25:31 -0300

    fix render port

--- app.py
+++ app.py
@@ -295,4 +295,6 @@ def eliminar_cobro(id):
     return redirect("/cobros")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
