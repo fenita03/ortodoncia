eec509e04bc1efc74233ccec290ba29253457081
fenita.03 <facunico145@gmail.com>
2026-04-16 19:30:17 -0300

    fix final render

--- app.py
+++ app.py
@@ -293,8 +293,7 @@ def eliminar_cobro(id):
     conn.close()
 
     return redirect("/cobros")
+import os
 
 if __name__ == "__main__":
-    import os
-
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
